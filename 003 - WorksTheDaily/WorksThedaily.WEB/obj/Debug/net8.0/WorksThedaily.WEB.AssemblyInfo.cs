f32a69a93ade2de8a57739391551636f1614972d
FabioNabor <fabionabor.bcard@gmail.com>
2024-05-24 06:48:32 -0300

    WebTaks

--- 003 - WorksTheDaily/WorksThedaily.WEB/obj/Debug/net8.0/WorksThedaily.WEB.AssemblyInfo.cs
+++ 003 - WorksTheDaily/WorksThedaily.WEB/obj/Debug/net8.0/WorksThedaily.WEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorksThedaily.WEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99a1c71eba19bd1ba0e15334f66fe1d807ec1ff6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c66691bbc3d87b9aa7d60ca1451b5bfd9174d72")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorksThedaily.WEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorksThedaily.WEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
